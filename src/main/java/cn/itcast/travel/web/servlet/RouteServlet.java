8936ac8f010d01f03487e85ec4f07dde2c5d6ec8
heqi <1104735883@qq.com>
2021-01-05 16:24:51 +0800

    旅游搜索功能实现

--- src/main/java/cn/itcast/travel/web/servlet/RouteServlet.java
+++ src/main/java/cn/itcast/travel/web/servlet/RouteServlet.java
@@ -23,10 +23,12 @@ public class RouteServlet extends BaseServlet {
         String currentPageStr = request.getParameter("currentPage");
         String pageSizeStr = request.getParameter("pageSize");
         String cidStr = request.getParameter("cid");
-
+        //接受rname 线路名称
+        String rname = request.getParameter("rname");
+        rname = new String(rname.getBytes("iso-8859-1"),"utf-8");
         int cid = 0;//类别id
         //2 处理参数
-        if(cidStr !=null && cidStr.length()>0){
+        if(cidStr != null && cidStr.length() > 0 && !"null".equals(cidStr)){
             cid = Integer.parseInt(cidStr);
         }
         int currentPage = 0;//当前页码，如果不传递，则默认为第一页
@@ -43,7 +45,7 @@ public class RouteServlet extends BaseServlet {
         }
 
         //3 调用service 查询pageBean 对象
-        PageBean<Route> pb = routeService.pageQuery(cid, currentPage, pageSize);
+        PageBean<Route> pb = routeService.pageQuery(cid, currentPage, pageSize,rname);
 
         //4. 将pageBean对象序列化为json，返回
         writeValue(pb,response);
